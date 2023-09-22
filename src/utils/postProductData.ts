31c517748a200e6ecfa03886d5dd780152a9a360
gdwmw <gededewowahyumw@gmail.com>
2023-09-22 22:47:48 +0700

    commit

--- src/utils/postProductData.ts
+++ src/utils/postProductData.ts
@@ -1,4 +1,14 @@
-export async function postProductData(data: any) {
+type postProductDataProps = {
+  id: number;
+  productName: string;
+  productCategory: string;
+  productFreshness: string;
+  productImage: string | null;
+  additionalDescription: string;
+  randomNumber: number;
+};
+
+export async function postProductData(data: postProductDataProps) {
   const res = await fetch("https://650c816247af3fd22f67b58e.mockapi.io/ProductData", {
     method: "POST",
     headers: {
