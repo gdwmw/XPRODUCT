3b1eed46fb2636027ebb8c95cd01547910cd4650
gdwmw <gededewowahyumw@gmail.com>
2023-09-29 21:06:10 +0700

    delete - Product Detail

--- src/app/productdetail/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Product Detail",
-  description: "© 2023 Gede Dewo Wahyu M.W. All rights reserved.",
-};
-
-export default function ProductDetailLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- src/app/productdetail/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <p className="text-3xl font-semibold">Loading...</p>
-    </div>
-  );
-}
