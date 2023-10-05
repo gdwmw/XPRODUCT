bba0c5e1c187656a658c9cc9e289ca1312bb9765
gdwmw <gededewowahyumw@gmail.com>
2023-10-05 16:54:20 +0700

    style - Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,7 +21,7 @@ export default function Login() {
   const session = useSession();
 
   // USE ROUTER
-  const router:AppRouterInstance = useRouter();
+  const router: AppRouterInstance = useRouter();
 
   // USERNAME & PASSWORD STATE
   const [username, setUsername] = useState<string>("");
@@ -42,7 +42,7 @@ export default function Login() {
     e.preventDefault();
     setLoading(true);
     try {
-      const res: SignInResponse|undefined = await signIn(`credentials`, {
+      const res: SignInResponse | undefined = await signIn(`credentials`, {
         username,
         password,
         redirect: false,
@@ -69,61 +69,57 @@ export default function Login() {
   }, [session, router]);
 
   return (
-    <div className="bg-white sm:bg-gray-100">
-      <div className="container mx-auto flex h-screen w-screen items-center justify-center">
-        <div className="w-[450px] rounded-xl bg-white p-4 sm:shadow-md">
-          <header className="flex flex-col items-center justify-center gap-5 pb-5 sm:pt-4">
-            <Image src={imgXPRODUCT} width={150} height={0} alt="Tailwind" />
-            <h2 className="text-center text-2xl font-bold text-tailwindBlue">{lang[code].header.title}</h2>
-          </header>
+    <div className="h-screen w-screen bg-white">
+      <div className="container mx-auto flex h-full w-full flex-col items-center justify-center gap-5 px-5">
+        <header className="flex flex-col items-center justify-center gap-5">
+          <Image src={imgXPRODUCT} width={150} height={0} alt="Tailwind" />
+          <h2 className="text-center text-2xl font-bold text-tailwindBlue">{lang[code].header.title}</h2>
+        </header>
 
-          <main>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-              {/* USERNAME */}
-              <InputText
-                label={lang[code].main.input.i1}
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                onClick={() => {}}
-                classBoolean={warning}
-              />
+        <main>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+            {/* USERNAME */}
+            <InputText
+              label={lang[code].main.input.i1}
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              onClick={() => {}}
+              classBoolean={warning}
+            />
 
-              {/* PASSWORD */}
-              <InputPassword
-                label={lang[code].main.input.i2}
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onClick={() => {}}
-                classBoolean={warning}
-              />
+            {/* PASSWORD */}
+            <InputPassword
+              label={lang[code].main.input.i2}
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onClick={() => {}}
+              classBoolean={warning}
+            />
 
-              {/* WARNING */}
-              <div className="text-center">
-                <Warning label={lang[code].main.warning} displayBoolean={warning} />
-              </div>
+            {/* WARNING */}
+            <div className="text-center">
+              <Warning label={lang[code].main.warning} displayBoolean={warning} />
+            </div>
 
-              {/* LOGIN BUTTON */}
-              <button
-                type="submit"
-                className={`w-full rounded-md ${loading ? "cursor-wait bg-tailwindGreenSecondary/50" : "bg-tailwindGreen"} px-4 py-2 text-white ${
-                  loading ? "" : "hover:bg-tailwindGreenSecondary"
-                } focus:outline-none`}
-                disabled={loading}
-              >
-                <div className="flex items-center justify-center gap-1">
-                  Login
-                  {loading && <Image src={loadingAnimation} alt="Loading" width={20} height={0} loading="eager" />}
-                </div>
-              </button>
-            </form>
-          </main>
+            {/* LOGIN BUTTON */}
+            <button
+              type="submit"
+              className={`flex w-full items-center justify-center gap-1 rounded-md ${
+                loading ? "cursor-wait bg-tailwindGreenSecondary/50" : "bg-tailwindGreen"
+              } px-4 py-2 text-white ${loading ? "" : "hover:bg-tailwindGreenSecondary"} focus:outline-none`}
+              disabled={loading}
+            >
+              <span>Login</span>
+              {loading && <Image src={loadingAnimation} alt="Loading" width={20} height={0} loading="eager" />}
+            </button>
+          </form>
+        </main>
 
-          <footer className="mt-6 pb-4 pt-1 text-center text-gray-500">
-            <p>{lang[code].footer.copyright}</p>
-          </footer>
-        </div>
+        <footer className="mt-3 text-center text-gray-400">
+          <p>{lang[code].footer.copyright}</p>
+        </footer>
       </div>
     </div>
   );
