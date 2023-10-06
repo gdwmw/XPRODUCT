3a71e09e45162bb5fda7f0d23ea891d5cc4525a5
gdwmw <gededewowahyumw@gmail.com>
2023-10-06 17:50:17 +0700

    update - Layout Create Product

--- src/app/createproduct/layout.tsx
+++ src/app/createproduct/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "XProduct | Admin",
+  title: "XProduct | Create Product",
   description: "© 2023 Gede Dewo Wahyu M.W. All rights reserved.",
 };
 
